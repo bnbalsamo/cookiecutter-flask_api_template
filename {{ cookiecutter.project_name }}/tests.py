4911bf596368dfc2f85eb3506102be5bff183bd1
brian <balsamo@uchicago.edu>
2017-08-25 16:41:09 -0500

    I lied about not needing the github username anymore - it's required to make the travis and coveralls links, so added that back in. Wrote up a quick test for checking that the version endpoint works now. Also changed it so that tests pass after initial build, leaving it up to the builder if they want to employ a more TDD type approach

--- {{ cookiecutter.project_name }}/tests.py
+++ {{ cookiecutter.project_name }}/tests.py
@@ -1,13 +1,27 @@
 import unittest
+import json
 from os import environ
 
 # Defer configuration to the tests setUp()
-environ['{{ cookiecutter.slug_name|upper }}_DEFER_CONFIG'] = True
+environ['{{ cookiecutter.slug_name|upper }}_DEFER_CONFIG'] = "True"
 
+import {{ cookiecutter.slug_name }}
 
 class Tests(unittest.TestCase):
-    def testFail(self):
-        self.assertTrue(False)
+    def setUp(self):
+        # Do any configuration/setting of mock env vars here
+        self.app = {{ cookiecutter.slug_name }}.app.test_client()
+
+    def testVersion(self):
+        version_response = self.app.get("/version")
+        self.assertEqual(version_response.status_code, 200)
+        version_json = json.loads(version_response.data.decode())
+        api_reported_version = version_json['version']
+        self.assertEqual(
+            {{ cookiecutter.slug_name }}.blueprint.__version__,
+            api_reported_version
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
